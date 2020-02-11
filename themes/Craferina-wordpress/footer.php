a024b313a3dc08be8cf91b93ef34fa2baaa8d954
Disha Shah <disha.shah1198@gmail.com>
2020-02-10 23:31:12 -0500

    Added content to footer

--- themes/Craferina-wordpress/footer.php
+++ themes/Craferina-wordpress/footer.php
@@ -14,18 +14,87 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ecommerce-theme' ) ); ?>">
+		<div class="site-info-01">
+			<a href="<?php echo esc_url( __( '', 'ecommerce-theme' ) ); ?>">
+			<h5> Crafterina </h5>
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'ecommerce-theme' ), 'WordPress' );
+				printf( esc_html__( 'About %s'), '' );
+				?><br/>
+				<?php
+				printf( esc_html__( 'Contact Us %s'), '' );
+				?><br/>
+				<?php
+				printf( esc_html__( 'Carrers %s'), '' );
 				?>
 			</a>
-			<span class="sep"> | </span>
+			<span class="sep">  </span>
+				
+		</div><!-- .site-info -->
+
+
+		<div class="site-info-02">
+			<a href="<?php echo esc_url( __( '', 'ecommerce-theme' ) ); ?>">
+			<h5> Deals </h5>
 				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'ecommerce-theme' ), 'Craferina', '<a href="/">Craferina</a>' );
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Coupons %s'), '' );
+				?><br/>
+				<?php
+				printf( esc_html__( 'Products to discover %s'), '' );
+				?><br/>
+				<?php
+				printf( esc_html__( 'Occasional deals %s'), '' );
 				?>
+			</a>
+			<span class="sep">  </span>
+				
+		</div><!-- .site-info -->
+		
+		<div class="site-info-03">
+			<a href="<?php echo esc_url( __( '', 'ecommerce-theme' ) ); ?>">
+			<h5> Policies </h5>
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Return %s'), '' );
+				?><br/>
+				<?php
+				printf( esc_html__( 'Exchange %s'), '' );
+				?><br/>
+				<?php
+				printf( esc_html__( 'Terms of use %s'), '' );
+				?><br/>
+				<?php
+				printf( esc_html__( 'Security %s'), '' );
+				?><br/>
+				<?php
+				printf( esc_html__( 'Privacy %s'), '' );
+				?>
+			</a>
+			<span class="sep">  </span>
+				
+		</div><!-- .site-info -->
+		
+
+
+
+
+		<div class="site-info-01">
+			<a href="<?php echo esc_url( __( '', 'ecommerce-theme' ) ); ?>">
+			<h5> Contact Info </h5>
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( '2000 Simcoe street, Oshawa, Ontario %s'), '' );
+				?><br/>
+				<?php
+				printf( esc_html__( '9876543210 %s'), '' );
+				?><br/>
+				<?php
+				printf( esc_html__( 'jewellery@craferina.com %s'), '' );
+				?>
+			</a>
+			<span class="sep">  </span>
+				
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
