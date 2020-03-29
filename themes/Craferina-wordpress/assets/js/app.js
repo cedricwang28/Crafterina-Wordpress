849f865dd0b648f5235462efaaa1d8c2c49091a9
tianwei <464231692@qq.com>
2020-03-29 00:57:12 -0400

    menu toggle

--- themes/Craferina-wordpress/assets/js/app.js
+++ themes/Craferina-wordpress/assets/js/app.js
@@ -5,11 +5,13 @@ window.onload=function(){
     let flag = 0;
 
     document.querySelector('.fa-bars').addEventListener('click',function(){
-        flag==0?flag=1:flag=0;
+        
         if(flag){
             document.querySelector('#primary-menu').style.display = 'none';
+            flag==0?flag=1:flag=0;
         }else{
             document.querySelector('#primary-menu').style.display = 'block';
+            flag==0?flag=1:flag=0;
         }
         
     });
